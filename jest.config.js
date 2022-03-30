69bfbb6805297f775982f92b7c88f746d9337dcd
Theo Sun <theo.sun@outlook.com>
2022-03-30 16:37:21 +0800

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+
+/**
+ * @type {import("@jest/types").Config.ProjectConfig}
+ */
+module.exports = {
+  testTimeout: 10 * 1000,
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!**/node_modules/**"
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules/",
+  ],
+  testEnvironment: "node",
+  testRegex: "/test/.*\\.test\\.ts$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json"
+  ]
+};
